df2f9ba9aff82c8a18cc91e956676b141fe220c1
Konstantin <Kostick_zenin@mai.ru>
2025-03-26 18:14:33 +0300

    hw_37: Добавил urls для landing page и core приложения

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('thanks/', views.thanks, name='thanks'),
+    path('orders/', views.orders_list, name='orders_list'),
+    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+]
