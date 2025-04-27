91b208bfd108f27994c520dd083b81a8c7138d42
Konstantin <Kostick_zenin@mai.ru>
2025-04-27 15:33:05 +0300

    hw_39: feat  Добавил модель Order с необходимыми полями и связями
    Добавлена модель Order со следующими полями:
    
    - client_name: Имя клиента (CharField)
    - phone: Телефон (CharField)
    - comment: Комментарий (TextField)
    - status: Статус (CharField) с предопределенными вариантами выбора
    - date_create: Дата создания (DateTimeField)
    - date_update: Дата обновления (DateTimeField)
    - master: ForeignKey к модели Master
    - services: ManyToManyField к модели Service
    - appointment_date: Дата и время записи (DateTimeField)
    
    Добавлены verbose_name для полей и Meta класс для отображения в админке.
    Добавлен метод __str__ для удобного представления объекта.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
+import re
+from django import db
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+
+    STATUS_CHOICES = [
+        ("not_approved", "Не подтвержден"),
+        ("moderated", "Прошел модерацию"),
+        ("spam", "Спам"),
+        ("approved", "Подтвержден"),
+        ("in_awaiting", "В ожидании"),
+        ("completed", "Завершен"),
+        ("canceled", "Отменен"),
+    ]
+
+    client_name = models.CharField(max_length=100, verbose_name="Имя клиента")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    comment = models.TextField(verbose_name="Комментарий", blank=True)
+    status = models.CharField(max_length=50, verbose_name="Статус", choices=STATUS_CHOICES, default="not_approved")
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания", db_index=True)
+    date_update = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    master = models.ForeignKey("Master", on_delete=models.SET_NULL,null=True, blank=True, verbose_name="Мастер")
+    services = models.ManyToManyField("Service", verbose_name="Услуги", related_name='orders')
+    appointment_date = models.DateTimeField(verbose_name="Дата и время записи")
+
+    def __str__(self):
+        return f"Заказ №{self.id} от {self.client_name}"
+    
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        ordering = ["-date_created"]
