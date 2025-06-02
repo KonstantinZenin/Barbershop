040a99c907fb86fce74727c18c324e907db4afaf
Konstantin <Kostick_zenin@mai.ru>
2025-06-02 19:42:26 +0300

    hw_42: feat: Добавлена форма для создания отзывов
    Добавлена форма `ReviewForm` для создания и отображения отзывов клиентов. Форма включает поля для имени клиента, текста отзыва, рейтинга, выбора мастера и загрузки фотографии.
    
    - Добавлено поле `rating` с валидаторами `MinValueValidator` и `MaxValueValidator`.
    - Добавлены виджеты для стилизации полей формы.
    - Добавлена фильтрация queryset для поля `master`, чтобы отображать только активных мастеров.

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
+
 from django import forms
-from django.core.exceptions import ValidationError
-from .models import Service
+from .models import Review, Master, Service
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ServiceForm(forms.ModelForm):
@@ -10,3 +11,44 @@ class ServiceForm(forms.ModelForm):
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3}),
         }
+
+
+
+class ReviewForm(forms.ModelForm):
+    rating = forms.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        widget=forms.HiddenInput(),
+        required=True
+    )
+    
+    class Meta:
+        model = Review
+        fields = ['client_name', 'text', 'rating', 'master', 'photo']
+        widgets = {
+            'client_name': forms.TextInput(attrs={
+                'class': 'form-control quantum-input',
+                'placeholder': 'Ваше имя'
+            }),
+            'text': forms.Textarea(attrs={
+                'class': 'form-control quantum-input',
+                'placeholder': 'Ваш отзыв...',
+                'rows': 4
+            }),
+            'master': forms.Select(attrs={
+                'class': 'form-select quantum-select',
+                'id': 'id_master'
+            }),
+            'photo': forms.FileInput(attrs={
+                'class': 'form-control quantum-file-input'
+            })
+        }
+        labels = {
+            'client_name': 'Имя клиента',
+            'text': 'Текст отзыва',
+            'master': 'Мастер',
+            'photo': 'Фото (необязательно)'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['master'].queryset = Master.objects.filter(is_active=True)
